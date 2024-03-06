8819e1172b38d5ca92353966a643703abba63cd5
Alex Deyanov <deyanovan@yandex.ru>
2024-03-06 15:44:49 +0300

    Task 2 is done

--- src/test/java/ru/netology/unit/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/unit/CashbackHackServiceTest.java
@@ -2,6 +2,7 @@ package ru.netology.unit;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
 
@@ -33,5 +34,35 @@ public class CashbackHackServiceTest {
         int actual = service.remain(1000);
 
         Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ifAmountLess1000Test2() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 1;
+        int actual = service.remain(999);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ifAmountMore1000Test2() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 999;
+        int actual = service.remain(1001);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ifAmountIs1000Test2() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
